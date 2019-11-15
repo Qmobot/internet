7ec322f21da49280dad1c1da02e4d00a8d103e71
biggybaron <dr.cleverest@gmail.com>
2019-11-15 15:36:15 +0600

    Added homework

--- boards/hwcode.cpp
+++ boards/hwcode.cpp
@@ -1,16 +1,15 @@
 // Данный кусочек кода отвечает за внедрения "Шапки", в которой прописаны детали библиотеки
 #include "qmobot.h"
 
+int line = 0;
+int vals[6] = {-1, -1, -1, -1, -1, -1};
 
 // Есть два вида комментариев:
 // 1. Комментарии, которые начинаются и идут на всю линию с "// Comment"
 // 2. Комментарии на отрывок, которые идут внутри скобок "/* Comment */"
 
 // Декларируем все функции, чтобы программа знала, что такие функции есть
-void displayIt(String t); 
-void chat(); 
 int minusSum(int n, int player);
-void primer();
 
 // Настройка чипа
 void setup(){
@@ -19,15 +18,47 @@ void setup(){
       /* Нужно ли подключать экран - Да */true, 
       /* Нужно ли подключение по последовательному порту? - Да */true
         );
-  
-  primer();
-  chat();
-  displayIt("I love QMOBOT");
 }
 
 // Данная часть кода будет выпонятся постоянно в цикле
 void loop(){
-    
+    Serial.print("Введите число: ");
+    String value;
+    while(Serial.available() == 0) {}
+    while(Serial.available()){
+      value = Serial.readString();
+      Serial.print("Рекурсия до ");
+      int ba = value.toInt();
+      Serial.print(ba);
+      Serial.print(": ");
+      Serial.println("====================================");\
+      int val = minusSum(ba, 0);
+
+        if(line > 5){
+          line = 6;
+          for(int i = 1; i < 6; i++){
+            vals[i-1] = vals[i];
+          }
+          vals[5] = val;
+        }
+        else{
+          vals[line] = val;
+          line ++;
+        }
+        Qchip.display->clear();
+        String text;
+        for(int i = 0; i < 6; i++){
+          if(vals[i]>=0){
+            text = String(vals[i]);
+            Qchip.display->drawString(0, i*10, text);
+          }
+        }
+        Qchip.display->display();
+
+
+      Serial.println(val);
+      Serial.println("====================================");
+    }
 }
 
 // Создаем функцию чата:
@@ -58,45 +89,4 @@ int minusSum(int n, int player){
   Serial.println(n);
   Serial.println();
   return n;
-}
-
-void primer(){
-  Serial.println("====================================");
-  Serial.println("Пример с 6-ю игроками:");
-  int summ = minusSum(6, 0);
-  Serial.println("====================================");
-  Serial.println();
-}
-
-void chat(){
-  Serial.println();
-  Serial.println("Функция обратной прогрессии: ");
-  Serial.println();
-
-  while(true){
-    Serial.print("Введите число: ");
-    String value;
-    while(Serial.available() == 0) {}
-    while(Serial.available()){
-      value = Serial.readString();
-      Serial.print("Рекурсия до ");
-      int ba = value.toInt();
-      Serial.print(ba);
-      Serial.print(": ");
-      Serial.println("====================================");
-      Serial.println(minusSum(ba, 0));
-      Serial.println("====================================");
-    }
-
-    break; // Закрытие цикла не через условие осуществляется с помощью break
-  }
-}
-
-void displayIt(String t){
-  Qchip.display->clear(); // Очистить дисплей от надписей
-                          // Пиксели по x,      пиксели по y,     текст
-  Qchip.display->drawString( 0 /*от 0 до 128*/, 0 /*от 0 до 64*/, t); // Написать текст на дисплее
-  Qchip.display->drawString(0, 63, "y");
-  Qchip.display->drawString(127, 0, "x");
-  Qchip.display->display(); // Показать на дисплее
 }
